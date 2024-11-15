a8666f408362c8e6f1915f07917fd116e0a1f7d2
sjmmics <sjmmics@gmail.com>
2024-11-16 07:08:28 +0900

    feat : 자판기 판매 상품 담당하는 클래스 추가

--- src/main/java/vendingmachine/model/Product.java
+++ src/main/java/vendingmachine/model/Product.java
@@ -1,4 +1,32 @@
 package vendingmachine.model;
 
+import java.util.Objects;
+
 public class Product {
+
+    public static final int PRICE_MIN = 100;
+
+    public static final int PRICE_UNIT = 10;
+
+    private final String name;
+
+    private final int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
 }
