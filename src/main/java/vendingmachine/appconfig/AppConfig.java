18df8b5c336a86b1d8fd0b5170b1c60e084a0086
sjmmics <sjmmics@gmail.com>
2024-11-15 20:01:49 +0900

    feat : ($AppConfig) controller, service 의존관계 설정 및 객체 생성 기능 추가

--- src/main/java/vendingmachine/appconfig/AppConfig.java
+++ src/main/java/vendingmachine/appconfig/AppConfig.java
@@ -1,4 +1,31 @@
 package vendingmachine.appconfig;
 
+import vendingmachine.controller.VMController;
+import vendingmachine.repository.VMRepository;
+import vendingmachine.service.VMService;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
 public class AppConfig {
+
+    private VMRepository repository() {
+        return new VMRepository();
+    }
+
+    private VMService service() {
+        return new VMService(repository());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    public VMController getVMController() {
+        return new VMController(inputView(), outputView(), service());
+    }
+
 }
