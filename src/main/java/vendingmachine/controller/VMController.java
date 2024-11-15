3d634b89f137373ccffe0385a9a317ba33e1d77c
sjmmics <sjmmics@gmail.com>
2024-11-15 20:01:09 +0900

    feat : 컨트롤러, 서비스 의존관계 추가
    서비스는 리파지토리와 의존
    
    콘트롤러는 서비스, 인풋 뷰, 아웃풋 뷰와 의존

--- src/main/java/vendingmachine/controller/VMController.java
+++ src/main/java/vendingmachine/controller/VMController.java
@@ -1,4 +1,21 @@
 package vendingmachine.controller;
 
+import vendingmachine.service.VMService;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
 public class VMController {
+
+    private final InputView inputView;
+
+    private final OutputView outputView;
+
+    private final VMService service;
+
+    public VMController(InputView inputView, OutputView outputView, VMService service) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.service = service;
+    }
+
 }
--- src/main/java/vendingmachine/service/VMService.java
+++ src/main/java/vendingmachine/service/VMService.java
@@ -1,4 +1,13 @@
 package vendingmachine.service;
 
+import vendingmachine.repository.VMRepository;
+
 public class VMService {
+
+    private final VMRepository repository;
+
+    public VMService(VMRepository repository) {
+        this.repository = repository;
+    }
+
 }
