cf1657406ce396207504c4cd12a8448527d8af19
sjmmics <sjmmics@gmail.com>
2024-11-16 13:10:11 +0900

    feat : ($OutputView) 출력 기능 빈줄 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -7,6 +7,7 @@ import vendingmachine.util.message.IOMessage;
 public class OutputView {
 
     public void printCoins(Coins coins) {
+        System.out.println();
         System.out.println(IOMessage.COINS_VM_HAVE);
         System.out.println(coins);
     }
