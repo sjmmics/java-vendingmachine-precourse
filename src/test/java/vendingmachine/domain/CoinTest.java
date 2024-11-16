8fdb678cc239c6fe5e4ba063d8cd9610f460dc60
sjmmics <sjmmics@gmail.com>
2024-11-16 13:15:05 +0900

    test : ($CoinTest) 동전 도메인 테스트 추가

---
+++ src/test/java/vendingmachine/domain/CoinTest.java
@@ -0,0 +1,33 @@
+package vendingmachine.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import vendingmachine.model.Coin;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CoinTest {
+
+    @ParameterizedTest
+    @MethodSource("coinAmountAndCoinType")
+    @DisplayName("코인 금액으로 코인 객체 가져오기 테스트")
+    void getCoinTypeByAmountTest(int amount, Coin expectedCoin) {
+        Coin coin = Coin.getFromAmount(amount);
+        assertEquals(coin, expectedCoin);
+
+    }
+
+    static Stream<Arguments> coinAmountAndCoinType() {
+        return Stream.of(
+                Arguments.arguments(10, Coin.COIN_10),
+                Arguments.arguments(50, Coin.COIN_50),
+                Arguments.arguments(100, Coin.COIN_100),
+                Arguments.arguments(500, Coin.COIN_500)
+        );
+    }
+
+}
