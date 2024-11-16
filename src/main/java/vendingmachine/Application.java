8cf7f5c24c9625b27a244a516b791e80a990b886
sjmmics <sjmmics@gmail.com>
2024-11-16 13:14:23 +0900

    feat : ($Application) 프로그램 시작점 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,12 @@
 package vendingmachine;
 
+import vendingmachine.appconfig.AppConfig;
+import vendingmachine.controller.VMController;
+
 public class Application {
     public static void main(String[] args) {
-
+        AppConfig appConfig = new AppConfig();
+        VMController controller = appConfig.getVMController();
+        controller.run();
     }
 }
